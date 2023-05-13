44969df405a313b3dfed187cff83279414bb023b
Reimnop <rayngu2005@gmail.com>
2023-05-14 01:29:29 +0700

    Implement HalfMaid.Async

--- FlexFramework/Core/Entities/Entity.cs
+++ FlexFramework/Core/Entities/Entity.cs
@@ -5,73 +5,7 @@ namespace FlexFramework.Core.Entities;
 
 public abstract class Entity : IUpdateable
 {
-    private HashSet<Coroutine> coroutines = new HashSet<Coroutine>();
-    private List<Coroutine> finishedCoroutines = new List<Coroutine>();
-
-    private float deltaTime;
     private bool started = false;
-
-    #region Coroutine Stuff
-    
-    private bool MoveNext(IEnumerator coroutine)
-    {
-        if (coroutine.Current is IEnumerator subroutine)
-        {
-            if (MoveNext(subroutine))
-            {
-                return true;
-            }
-        }
-
-        return coroutine.MoveNext();
-    }
-
-    protected Coroutine StartCoroutine(IEnumerator enumerator)
-    {
-        Coroutine coroutine = new Coroutine(enumerator);
-        coroutines.Add(coroutine);
-        return coroutine;
-    }
-
-    protected void StopCoroutine(Coroutine coroutine)
-    {
-        coroutines.Remove(coroutine);
-    }
-    
-    protected IEnumerator WaitForEndOfFrame()
-    {
-        yield return null;
-    }
-
-    protected IEnumerator WaitForFrames(int frames)
-    {
-        int i = 0;
-        while (i < frames)
-        {
-            i++;
-            yield return null;
-        }
-    }
-
-    protected IEnumerator WaitForSeconds(float time)
-    {
-        float t = 0.0f;
-        while (t < time)
-        {
-            t += deltaTime;
-            yield return null;
-        }
-    }
-
-    protected IEnumerator WaitUntil(Func<bool> predicate)
-    {
-        while (!predicate())
-        {
-            yield return null;
-        }
-    }
-    
-    #endregion
 
     public virtual void Start()
     {
@@ -84,22 +18,5 @@ public abstract class Entity : IUpdateable
             started = true;
             Start();
         }
-        
-        deltaTime = args.DeltaTime;
-
-        foreach (Coroutine coroutine in coroutines)
-        {
-            if (!MoveNext(coroutine.InternalRoutine))
-            {
-                finishedCoroutines.Add(coroutine);
-            }
-        }
-
-        foreach (Coroutine coroutine in finishedCoroutines)
-        {
-            coroutines.Remove(coroutine);
-        }
-        
-        finishedCoroutines.Clear();
     }
 }
---
+++ FlexFramework/Core/Entities/InteractiveEntity.cs
@@ -0,0 +1,45 @@
+﻿using HalfMaid.Async;
+
+namespace FlexFramework.Core.Entities;
+
+/// <summary>
+/// Extension of Entity that adds task based async code support
+/// </summary>
+public class InteractiveEntity : Entity
+{
+    protected GameTaskRunner TaskRunner { get; }
+    protected float DeltaTime { get; private set; }
+
+    public override void Update(UpdateArgs args)
+    {
+        base.Update(args);
+        
+        DeltaTime = args.DeltaTime;
+
+        // Update tasks
+        TaskRunner.RunNextFrame();
+    }
+    
+    #region Task shenanigans
+
+    protected void StartTaskImmediately(Func<GameTask> task)
+    {
+        TaskRunner.StartImmediately(task);
+    }
+    
+    protected GameTaskYieldAwaitable WaitUntilNextFrame() => TaskRunner.Next();
+    protected GameTaskYieldAwaitable DelayFrames(int frames) => TaskRunner.Delay(frames);
+    protected ExternalTaskAwaitable RunTask(Func<Task> task) => TaskRunner.RunTask(task);
+
+    protected async GameTask WaitSeconds(float seconds)
+    {
+        float t = 0;
+        while (t < seconds)
+        {
+            t += DeltaTime;
+            await WaitUntilNextFrame();
+        }
+    }
+
+    #endregion
+}
