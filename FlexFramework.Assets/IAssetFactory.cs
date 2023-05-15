4c6a5c155fa6ed09d8e2b998e2dc8a3a51cbe2f7
Reimnop <rayngu2005@gmail.com>
2023-05-15 21:49:00 +0700

    get rid of unnecessary projects

--- FlexFramework.Assets/IAssetFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FlexFramework.Assets;
-
-public interface IAssetFactory
-{
-    string GetAssetTypeId(Type type);
-    void WriteAsset(object asset, Stream stream);
-    object ReadAsset(string type, Stream stream);
-}
